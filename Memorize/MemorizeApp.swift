f31fc2ed15189796ec3efdfda6bae4678581defa
Ryan Zubery <ryan.zubery@icloud.com>
2021-06-01 21:16:27 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ryan Zubery on 6/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
